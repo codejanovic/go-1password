a8026811598c330c78a47f38893338ece7cf9458
codejanovic <codejanovic@gmail.com>
2018-11-04 11:21:23 +0100

    cleaned up unused stuff from environment

--- environment/environment.go
+++ environment/environment.go
@@ -3,8 +3,6 @@ package environment
 import (
 	"path"
 
-	throw "github.com/codejanovic/go-1password/throw"
-	"github.com/docker/docker-credential-helpers/credentials"
 	homedir "github.com/mitchellh/go-homedir"
 )
 
@@ -13,22 +11,6 @@ type environment struct {
 	UserDirectory string
 	AppDirectory  string
 	SettingsFile  string
-	nativeStore   credentials.Helper
-}
-
-func (e *environment) AddCredentials(credentials *credentials.Credentials) {
-	err := e.nativeStore.Add(credentials)
-	if err != nil {
-		throw.Throw(err, "Did you deny credentials access to go-1password?")
-	}
-}
-
-func (e *environment) GetCredentials(identifier string) (string, string) {
-	user, secret, err := e.nativeStore.Get(identifier)
-	if err != nil {
-		throw.Throw(err, "Did you deny credentials access to go-1password?")
-	}
-	return user, secret
 }
 
 // Environment singleton
